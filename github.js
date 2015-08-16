95d616fcf1fa3dc7ac37fce5cb57b3538d7e3664
reuttal <reuttal@users.noreply.github.com>
2015-08-16 23:05:06 +0300

    Update github.js

--- github.js
+++ github.js
@@ -1,26 +1,14 @@
 var Client = require('node-rest-client').Client;
 client = new Client();
 
-getGitStatus = function(callback) {
+exports.getStatus = function(callback) {
 	client.get("https://status.github.com/api/status.json", function(data, response){
 		callback(data);
 	});
 }
 
-getGitAvailability = function(callback){
+exports.getAvailability = function(callback){
 	client.get("https://status.github.com/api/messages.json", function(data, response){
-		callback(data);
-	});
-}
-
-exports.getStatus = function(callback) {
-	getGitStatus(function(data, response){
-		callback(data);
-	});
-}
-
-exports.getAvailability = function(callback) {
-	getGitAvailability(function(data, response){
 		callback(data);
 	});
 }
